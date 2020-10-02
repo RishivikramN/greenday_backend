60223fb9f532578ffcd0496aa15e7c009d95d0f8
RishivikramN <rishi.vikram.1@gmail.com>
2020-10-02 21:27:01 +0530

    node backend update

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/products',products);
 
-const port = process.env.PORT || 6401;
+const port = process.env.PORT || 3000;
 
 app.listen(port,console.log(`Listening at port ${port}`));
--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,32 @@ const { Product } = require("../models/products");
 const router = express.Router();
 
 router.get('/',async (req,res)=>{
-    const product = await Product.find();
-    res.send(product);
+    try {
+        const filter = req.query.filter==="" ||req.query.filter===undefined ? ".*": req.query.filter;
+        const page = req.query.page===""||req.query.page===undefined ? 1 : parseInt(req.query.page);
+        const pagination = req.query.pagination===""||req.query.pagination===undefined ? 6 : parseInt(req.query.pagination);
+
+        const product = await Product.find({title:{$regex:`${filter}`,$options:"i"}})
+                                     .sort({price:req.query.sort})
+                                     .skip((page-1) * pagination)
+                                     .limit(pagination);
+        res.send(product);    
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+router.get('/count',async (req,res)=>{
+    try {
+        const count = await Product.estimatedDocumentCount();
+        result = {
+            count
+        };
+        res.send(result);  
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.post('/',async(req,res)=>{
